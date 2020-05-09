0fae4d32b9377d5e63ee3e5cdd9f4935e3755fa9
Richard Macaranas <rjmac22899@gmail.com>
2020-05-09 21:58:50 +0000

    Fixed bug in example2. result should be uint16_t

--- Examples/example2.c
+++ Examples/example2.c
@@ -9,7 +9,7 @@ uint16_t multiply(uint8_t a, uint8_t b) {
 
 int main(void) {
    uint8_t foo = 50, bar = 10;
-   uint8_t result;
+   uint16_t result;
    /* return val implicitly typecasted (bad) */
    result = multiply(foo, bar);
    printf("foo = % d bar = % d result = % d\n", foo, bar, result);
